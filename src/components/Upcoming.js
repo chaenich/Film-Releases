f77ec3a3eb31fb13c1a26d2f6d43a7684af1803b
Chae Nicholson <chae.nicholson@gmail.com>
2019-08-12 16:15:44 +0100

    Added component for upcoming films

---
+++ src/components/Upcoming.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Upcoming extends Component {
+  render() {
+    return (
+      <div className="Upcoming">
+        <h4>Coming Soon....</h4>
+      </div>
+    )
+  }
+}
+
+export default Upcoming;
--- src/containers/FilmBox.js
+++ src/containers/FilmBox.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import FilmList from '../components/FilmList'
+import Upcoming from '../components/Upcoming'
+import Separator from '../components/Separator'
 
 class FilmBox extends Component {
   constructor(props) {
@@ -11,8 +13,11 @@ class FilmBox extends Component {
   render () {
     return(
       <div className="film-box">
+        <Separator />
         <h2>Main film box</h2>
         <FilmList />
+        <Separator />
+        <Upcoming />
       </div>
     )
   }
