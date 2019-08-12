d050c67d796e1f38eabdf6db85e809c877f6dd71
Chae Nicholson <chae.nicholson@gmail.com>
2019-08-12 15:39:48 +0100

    Filmbox linkedpwd

---
+++ src/containers/FilmBox.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class FilmBox extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    }
+  }
+
+  render () {
+    return(
+      <div className="film-box">
+        <h2>Main film box</h2>
+      </div>
+    )
+  }
+
+}
+
+export default FilmBox;
